e52798ae2adf1911b25b09f7658b148206b6d45f
Alex Pan <panina.Alexa5@gmail.com>
2022-04-07 11:31:24 +0300

    homework 3. network

--- HomeWorkNetwork/app/src/main/java/com/pg/homeworknetwork/Models.kt
+++ HomeWorkNetwork/app/src/main/java/com/pg/homeworknetwork/Models.kt
@@ -1,5 +1,29 @@
 package com.pg.homeworknetwork
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
-class Movies
+@Serializable
+data class Movies (
+        @SerialName("page")
+        val page: Int,
+        @SerialName("results")
+        val movies: MutableList<Movie>
+)
 
-class Movie
+@Serializable
+data class Movie (
+        @SerialName("id")
+        val id: Int,
+        @SerialName("title")
+        val title: String,
+        @SerialName("poster_path")
+        val posterPath: String,
+        @SerialName("original_title")
+        val originalTitle: String?,
+        @SerialName("overview")
+        val overview: String?,
+        @SerialName("popularity")
+        val popularity: Double?,
+        @SerialName("release_date")
+        val releaseDate: String?
+)
